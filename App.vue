66a78901345c429dcf08ebdafa8148480e0c7bd5
Otabek Abdusalomov <103166280+otabekabdusalomov@users.noreply.github.com>
2022-12-11 09:54:25 +0300

    Initial commit

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+   <header>
+    <div class="container">
+        <h1>Hello Next</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, eveniet.</p>
+    </div>
+   </header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+
+</style>
