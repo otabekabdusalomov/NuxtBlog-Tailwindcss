f6dd5c7e28a40025db242534e7bfad502d7de74f
otabekabdusalomov <otabekabdusalamov2001@gmail.com>
2022-12-27 21:53:59 +0300

    Nuxt Blog

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>About page</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta pariatur nam hic voluptatum ad repellat debitis dignissimos voluptas fugiat quaerat vel, tempore eum magnam odit eveniet at beatae quia consectetur.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p {
+    margin: 20px 0;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <p>Product details for {{ id }}</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita est tempore esse maiores doloremque similique odit sunt ab minus vero.</p>
+    </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
