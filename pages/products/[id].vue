1245028361d3a97735d47ba41024d61c21a97c5c
otabekabdusalomov <otabekabdusalamov2001@gmail.com>
2023-01-03 20:31:27 +0300

    nuxt merch and blog

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,5 +1,10 @@
 <template>
     <div>
+     <Head>
+      <Title>Nuxt Blog | {{ product.title }}</Title>
+      <Meta name="description" :content="product.description" />
+     </Head>
+
        <ProductDetails :product="product" />
     </div>
 </template>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,19 +1,26 @@
 <template>
-    <div>
-      <div class="grid grid-cols-4 gap-5">
-        <div v-for="p in products">
-           <ProductCard :product="p" />
-        </div>
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products">
+        <ProductCard :product="p" />
       </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-  definePageMeta({
-    layout: 'products'
-  })
-  // fetch the products
- const { data: products } = await useFetch('https://fakestoreapi.com/products')
+definePageMeta({
+  layout: 'products'
+})
+// fetch the products
+const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
+useHead({
+  title: 'Nuxt Blog | Merch',
+  meta: [
+  { name: 'description', content: 'Nuxt 3 Merch' }
+  ]
+})
 </script>
 
 <style scoped>
