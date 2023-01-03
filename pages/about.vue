70f620b67cb857e6c8abd4d0c01254390593ddb2
otabekabdusalomov <otabekabdusalamov2001@gmail.com>
2023-01-03 23:25:55 +0300

    fetch api

--- pages/about.vue
+++ pages/about.vue
@@ -2,11 +2,12 @@
     <div>
         <h2>About page</h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta pariatur nam hic voluptatum ad repellat debitis dignissimos voluptas fugiat quaerat vel, tempore eum magnam odit eveniet at beatae quia consectetur.</p>
+        <div>{{ data }}</div>
     </div>
 </template>
 
 <script setup>
-
+  const { data } = await useFetch('/api/ninja?')
 </script>
 
 <style scoped>
